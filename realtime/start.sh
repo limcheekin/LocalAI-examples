2e5239e5baf2e81ec3c8cd76256ad42b10e34f11
limcheekin <limcheekin@vobject.com>
2025-11-02 11:32:03 +0800

    chore: add start.sh

---
+++ realtime/start.sh
@@ -0,0 +1,7 @@
+export OPENAI_API_KEY=your-api-key-here
+export OPENAI_MODEL=qwen2.5-omni-3b
+export OPENAI_TTS_VOICE=
+export OPENAI_TTS_MODEL=voice-en-us-amy-low
+export OPENAI_BASE_URL=http://192.168.1.111:8880/v1
+
+bash run.sh
